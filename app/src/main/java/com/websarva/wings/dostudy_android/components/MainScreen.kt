921f74268e8891b32af3c6ae350441afe1734ec5
ryohamag <Ryojune21@gmail.com>
2024-12-15 21:42:18 +0900

    背景をグラデーションに変更

--- app/src/main/java/com/websarva/wings/dostudy_android/components/MainScreen.kt
+++ app/src/main/java/com/websarva/wings/dostudy_android/components/MainScreen.kt
@@ -1,16 +1,20 @@
 package com.websarva.wings.dostudy_android.components
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.Text
@@ -22,6 +26,8 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.vectorResource
@@ -112,76 +118,96 @@ fun MainScreen(
         )
     }
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+    Box(
         modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxWidth()
-    ) {
-        val hour = if(!vm.isTimerMode) vm.seconds / 3600 else vm.selectedTimer?.div(3600) ?: 0
-        val minute = if(!vm.isTimerMode) (vm.seconds % 3600) / 60 else (vm.selectedTimer?.rem(3600) ?: 0) / 60
-        val second = if(!vm.isTimerMode) vm.seconds % 60 else vm.selectedTimer?.rem(60) ?: 0
-
-        Text(
-            text = "${hour.toString().padStart(2, '0')}h" +
-                    "${minute.toString().padStart(2, '0')}m" +
-                    "${second.toString().padStart(2, '0')}s",
-            modifier = Modifier.padding(64.dp),
-            fontSize = 52.sp
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(
-            onClick = {
-                httpRequest(username = vm.username, channelId = vm.channelId)
-                vm.isStudyStarted = !vm.isStudyStarted
-            },
-            modifier = Modifier.padding(16.dp),
-            shape = RoundedCornerShape(8.dp)
-        ) {
-            Text(
-                text = if (vm.isStudyStarted) "stop" else "start",
-                modifier = Modifier.padding(64.dp),
-                fontSize = 64.sp
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color.White,
+                        Color.Cyan,
+                        Color.Blue
+                    )
+                )
             )
-        }
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+        ) {
+            val hour = if(!vm.isTimerMode) vm.seconds / 3600 else vm.selectedTimer?.div(3600) ?: 0
+            val minute = if(!vm.isTimerMode) (vm.seconds % 3600) / 60 else (vm.selectedTimer?.rem(3600) ?: 0) / 60
+            val second = if(!vm.isTimerMode) vm.seconds % 60 else vm.selectedTimer?.rem(60) ?: 0
 
-        Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = "${hour.toString().padStart(2, '0')}h" +
+                        "${minute.toString().padStart(2, '0')}m" +
+                        "${second.toString().padStart(2, '0')}s",
+                modifier = Modifier.padding(64.dp),
+                fontSize = 52.sp
+            )
 
-        if (!vm.isStudyStarted) {
-            Row(
-                modifier = Modifier.padding(16.dp)
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {
+                    httpRequest(username = vm.username, channelId = vm.channelId)
+                    vm.isStudyStarted = !vm.isStudyStarted
+                },
+                modifier = Modifier.padding(16.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonColors(
+                    contentColor = Color.Cyan,
+                    containerColor = Color.White,
+                    disabledContentColor = Color.Gray,
+                    disabledContainerColor = Color.Gray
+                )
             ) {
-                Button(
-                    onClick = { vm.isSettingsDialogOpen = true },
-                    modifier = Modifier
-                        .weight(5f)
-                        .padding(16.dp),
-                    shape = RoundedCornerShape(8.dp)
-                ) {
-                    Icon(
-                        painter = rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.baseline_settings_24)),
-                        contentDescription = "設定ボタン"
-                    )
-                }
+                Text(
+                    text = if (vm.isStudyStarted) "stop" else "start",
+                    modifier = Modifier.padding(64.dp),
+                    fontSize = 64.sp
+                )
+            }
 
-                IconToggleButton(
-                    checked = vm.selectedTimer != null,
-                    onCheckedChange = {
-                        vm.isTimerMode = true
-                        if(vm.isTimerMode) navController.navigate("TimerSetting")
-                    },
-                    modifier = Modifier
-                        .weight(2f)
-                        .padding(16.dp)
-                        .scale(2f)
+            Spacer(modifier = Modifier.height(5.dp))
+
+            if (!vm.isStudyStarted) {
+                Row(
+                    modifier = Modifier.padding(16.dp)
                 ) {
-                    Icon(
-                        painter = rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.baseline_timer_24)),
-                        contentDescription = "タイマー設定ボタン"
-                    )
+                    Button(
+                        onClick = { vm.isSettingsDialogOpen = true },
+                        modifier = Modifier
+                            .weight(5f)
+                            .padding(16.dp),
+                        shape = RoundedCornerShape(8.dp)
+                    ) {
+                        Icon(
+                            painter = rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.baseline_settings_24)),
+                            contentDescription = "設定ボタン"
+                        )
+                    }
+
+                    IconToggleButton(
+                        checked = vm.selectedTimer != null,
+                        onCheckedChange = {
+                            vm.isTimerMode = true
+                            if(vm.isTimerMode) navController.navigate("TimerSetting")
+                        },
+                        modifier = Modifier
+                            .weight(2f)
+                            .padding(16.dp)
+                            .scale(2f)
+                    ) {
+                        Icon(
+                            painter = rememberVectorPainter(image = ImageVector.vectorResource(id = R.drawable.baseline_timer_24)),
+                            contentDescription = "タイマー設定ボタン"
+                        )
+                    }
                 }
             }
         }
