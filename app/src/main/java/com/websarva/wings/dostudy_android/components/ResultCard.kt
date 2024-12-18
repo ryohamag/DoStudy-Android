960fbc276052491ed4b41cc557531f9e208749f1
ryohamag <Ryojune21@gmail.com>
2024-12-18 16:31:40 +0900

    リザルト画面にて、失敗時はカードが赤くなるように設定

--- app/src/main/java/com/websarva/wings/dostudy_android/components/ResultCard.kt
+++ app/src/main/java/com/websarva/wings/dostudy_android/components/ResultCard.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -32,7 +33,8 @@ fun ResultCard(
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
         modifier = Modifier
             .padding(16.dp)
-            .fillMaxWidth(), colors = CardDefaults.cardColors(Color(0xffcce6ff))
+            .fillMaxWidth(),
+        colors = if(resultDataTable.status) CardDefaults.cardColors(Color(0xffcce6ff)) else CardDefaults.cardColors(MaterialTheme.colorScheme.errorContainer)
     ) {
         Column(
             modifier = Modifier.fillMaxWidth()
