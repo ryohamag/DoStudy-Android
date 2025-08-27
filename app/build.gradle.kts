6f40126a41bc8fe6a6af8b6f1da7554a5fba2645
ryohamag <Ryojune21@gmail.com>
2025-08-27 22:23:41 +0900

    ver.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.websarva.wings.dostudy_android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.0"
+        versionCode = 5
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
